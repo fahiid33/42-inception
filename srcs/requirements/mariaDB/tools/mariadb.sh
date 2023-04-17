6bac3c5874d4651c42477f15ea73ac92f151add3
fahiid33 <trey34809@gmail.com>
2023-04-17 01:54:46 +0000

    mariadb done

--- srcs/requirements/mariaDB/tools/mariadb.sh
+++ srcs/requirements/mariaDB/tools/mariadb.sh
@@ -9,6 +9,8 @@ mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS \`$DB_USER\`
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO \`$DB_USER\`@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
+mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$MYSQL_ROOT_PASSWORD')"
+
 sleep 3
 
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown

00bdd3ea29c71d9586cfe6d8886984be7878a591
fahiid33 <trey34809@gmail.com>
2023-05-13 03:44:33 +0100

    ..>

--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
 NAME = inception
-USER=$(whoami)
 
 all : $(NAME)
 
 $(NAME) : 
-	sudo sh -c 'echo "127.0.1.1\tfstitou.42.fr"  >> /etc/hosts'
+	sudo sh -c 'echo "127.0.0.1\tfstitou.42.fr"  >> /etc/hosts'
 	sudo mkdir -p "/home/fstitou/data/wp_db"
 	sudo mkdir -p "/home/fstitou/data/wp_files"
 	docker compose -f ./srcs/docker-compose.yml up --build -d
---
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -0,0 +1,24 @@
+<?php
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
+define( 'DB_NAME', 'mariadb' );
+define( 'DB_USER', 'fstitou' );
+define( 'DB_PASSWORD', 'popo333');
+define( 'DB_HOST', 'mariadb');
+define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_COLLATE', '' );
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+require_once( ABSPATH . 'wp-settings.php' );
